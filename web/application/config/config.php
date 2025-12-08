97a0a66b7661d657090694507c89d981dfdc91db
itrerek <itrerek@gmail.com>
2025-12-08 05:47:02 +0000

    88

--- web/application/config/config.php
+++ web/application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '/web/';
+$config['base_url'] = 'https://core.my.id/web/';
+
 
 /*
 |--------------------------------------------------------------------------
