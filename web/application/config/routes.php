12390de2fd6fe8f9305b9b1faa44baa53b81ae97
itrerek <itrerek@gmail.com>
2025-12-22 00:09:57 +0000

    4

--- web/application/config/routes.php
+++ web/application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'MiniApp/index';
+$route['default_controller'] = 'MiniApp/dashboard';
 $route['miniapp'] = 'MiniApp/index';
 $route['miniapp/login/(:num)'] = 'MiniApp/index/$1';
 $route['miniapp/admin'] = 'MiniApp/admin_panel';
