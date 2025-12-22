c0674625f197275a5780370aad30644cf056dbb5
itrerek <itrerek@gmail.com>
2025-12-22 00:00:38 +0000

    1

--- web/application/views/templates/header.php
+++ web/application/views/templates/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo isset($title) ? $title : lang('teleXweb'); ?></title>
+    <title><?php echo isset($title) ? $title . ' | teleXweb' : 'teleXweb'; ?></title>
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
@@ -21,12 +21,43 @@
     <!-- Page content wrapper-->
     <div id="page-content-wrapper">
         <!-- Top navigation-->
-        <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+        <nav class="navbar navbar-expand-lg navbar-light bg-white border-bottom shadow-sm">
             <div class="container-fluid">
-                <button class="btn btn-primary" id="sidebarToggle">
+                <button class="btn btn-primary me-3" id="sidebarToggle">
                     <i class="fas fa-bars"></i>
                 </button>
+
+                <a class="navbar-brand fw-bold" href="<?php echo site_url(); ?>">teleXweb</a>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
+                        <li class="nav-item active"><a class="nav-link" href="<?php echo site_url('dashboard'); ?>">Dashboard</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Bantuan</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fas fa-user fa-fw me-1"></i>
+                                <?php echo $this->session->userdata('username') ?? 'Guest'; ?>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="<?php echo site_url('users/profile'); ?>">
+                                    <i class="fas fa-user-circle fa-fw me-2"></i>Profil
+                                </a>
+                                <a class="dropdown-item" href="<?php echo site_url('settings'); ?>">
+                                     <i class="fas fa-cog fa-fw me-2"></i>Pengaturan
+                                </a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="<?php echo site_url('auth/logout'); ?>">
+                                    <i class="fas fa-sign-out-alt fa-fw me-2"></i>Logout
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
         <!-- Page content-->
-        <div class="container-fluid">
+        <div class="container-fluid p-4">
